faa03dc27f1f26e0cd871937865ab4262d2f2439
Esuabom Dijemeni <e.dijemeni@gmail.com>
2023-10-17 08:52:34 +0100

    Completed Chair Factory

---
+++ factory/chair_factory_example/medium_chair.py
@@ -0,0 +1,17 @@
+"A Class of Chair"
+from interface_chair import IChair
+
+class MediumChair(IChair):
+    "The Medium Chair Concrete Class implements the IChair interface"
+
+    def __init__(self) -> None:
+        self._height = 60
+        self._width = 60
+        self._depth = 60
+
+    def get_dimensions(self):
+        return {
+            "width": self._height,
+            "depth": self._depth, 
+            "height": self._height
+        }
