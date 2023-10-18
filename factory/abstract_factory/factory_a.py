c12b7003311929a976e2bce91ce2ab9b455e2c5b
Esuabom Dijemeni <e.dijemeni@gmail.com>
2023-10-18 18:33:46 +0100

    Completed Furniture Factory implementation and client side implementation

---
+++ factory/abstract_factory/factory_a.py
@@ -0,0 +1,57 @@
+# pylint: disable=too-few-public-methods
+"FactorA Sample Code"
+from abc import ABCMeta, abstractmethod
+
+class IProduct(metaclass=ABCMeta):
+    "A hypothetical class interface (Product)"
+
+    @staticmethod
+    @abstractmethod
+    def create_object():
+        "An abstract interface method"
+        pass
+
+class ConcreteProductA(IProduct):
+    "A concrete class that implements the IProduct Interface"
+
+    def __init__(self) -> None:
+        self.name = "ConcreteProductA"
+
+    def create_object(self):
+        return self
+    
+class ConcreteProductB(IProduct):
+    "A concrete class that implements the IProduct Interface"
+
+    def __init__(self) -> None:
+        self.name = "ConcreteProductB"
+
+    def create_object(self):
+        return self
+    
+class ConcreteProductC(IProduct):
+    "A concrete class that implements the IProduct Interface"
+
+    def __init__(self) -> None:
+        self.name = "ConcreteProductC"
+
+    def create_object(self):
+        return self
+    
+class FactoryA:
+    "The Factory Class"
+
+    @staticmethod
+    def create_object(some_property):
+        "A static method to get a concrete product"
+        try:
+            if some_property == 'a':
+                return ConcreteProductA()
+            if some_property == 'b':
+                return ConcreteProductB()
+            if some_property == 'c':
+                return ConcreteProductC()
+            raise Exception('Class Not Found')
+        except Exception as _e:
+            print(_e)
+        return None 
--- factory/chair_factory_example/client.py
+++ factory/chair_factory_example/client.py
@@ -1,7 +1,15 @@
-"Factory Use Case Example Code"
+# "Factory Use Case Example Code"
 
-from chair_factory import ChairFactory
+# from chair_factory import ChairFactory
 
-# The Client
-CHAIR = ChairFactory.get_chair("BigChair")
-print(CHAIR.get_dimensions())
+# # The Client
+# CHAIR = ChairFactory.get_chair("BigChair")
+# print(CHAIR.get_dimensions())
+
+from furniture_factory import FurnitureFactory
+
+FURNITURE = FurnitureFactory.get_furniture("SmallChair")
+print(f"{FURNITURE.__class__}: {FURNITURE.get_dimensions()}")
+
+FURNITURE = FurnitureFactory.get_furniture("MediumTable")
+print(f"{FURNITURE.__class__}: {FURNITURE.get_dimensions()}")
